5a03e05ddec95367fe4b9b76a4bb9818700d9738
taal-toiv7 <kaloflavor@gmail.com>
2022-12-19 00:05:59 +0200

    tryme

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
 *
 * Return: void pointer to allocated memory
 */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 	unsigned int i;
